16313b282211052041b9e95b9749f962a570d560
Brian Hwang <hbros2004@gmail.com>
2023-11-15 23:01:05 +1100

    clean up the bytestring

--- test.py
+++ test.py
@@ -3,7 +3,10 @@ import subprocess
 # ubprocess.check_output(args, *, stdin=None, stderr=None, shell=False, universal_newlines=False)
 
 args = ['netsh','wlan','show','network']
-devices = subprocess.check_output(args=args)
+output =  subprocess.check_output(args=args)
 
+# output is a byte-string containing carriagereturn chars and newline chars. 
 
-print(devices)
+decodedUTF8 = output.decode('utf-8') 
+
+print(decodedUTF8)
