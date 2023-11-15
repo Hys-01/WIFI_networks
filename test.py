0a67bfdcf634c281e64f80b0e255c59e173e8dac
Brian Hwang <hbros2004@gmail.com>
2023-11-15 23:21:05 +1100

    adding markers in front of every new line

--- test.py
+++ test.py
@@ -6,7 +6,19 @@ args = ['netsh','wlan','show','network']
 output =  subprocess.check_output(args=args)
 
 # output is a byte-string containing carriagereturn chars and newline chars. 
-
+print(output)
 decodedUTF8 = output.decode('utf-8') 
 
 print(decodedUTF8)
+
+marked = ''
+marker_count = 0
+for x in decodedUTF8: 
+    marked = marked+x
+
+    if x =="\r": 
+        marker_count +=1
+        marked = marked+ ('$' + str(marker_count))
+
+
+print(marked)
