940bad98cc7c20ec7f7a8ee25a01291ae4283f14
Brian Hwang <hbros2004@gmail.com>
2023-11-16 22:28:14 +1100

    file for colour global vars, organised test.py`
    removed long for loop

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import subprocess
-
+from colors_custom import *
 # ubprocess.check_output(args, *, stdin=None, stderr=None, shell=False, universal_newlines=False)
 
 args = ['netsh','wlan','show','network']
@@ -7,29 +7,14 @@ output =  subprocess.check_output(args=args)
 
 # output is a byte-string containing carriagereturn chars and newline chars. 
 
-decodedUTF8 = output.decode('utf-8') 
+decoded_U8 = output.decode('utf-8') 
+decoded_U8 = decoded_U8
+lines = decoded_U8.split("\r") 
 
-marked = ''
-marker_count = 0
-numnetworks_notconsidered = True
-for x in decodedUTF8:
-
-    if x.isdigit() and numnetworks_notconsidered: 
-        marked = marked + "\033[32m" + x + "\033[0m"
-        numnetworks_notconsidered = False
-        continue
+print(lines)
+print(PLAIN)
 
 
-    marked = marked+x  
-
-    if x =="\r": 
-        marker_count +=1
-        if marker_count ==1: 
-            marked = marked + "\033[31m"
-        if marker_count==2:
-            marked = marked + "\033[0m"
-
-    
 
 
-print(marked)
+
